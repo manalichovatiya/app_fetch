8e567c39df3ee4ad85398976c35f0ece710ed0c7
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2024-01-17 16:48:39 +0530

    search and filter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+import { data } from './data.js'
+import { Container, Form, InputGroup, Table } from 'react-bootstrap';
 
 function App() {
-  const [result, setresult] = useState([]);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data);
-        setresult(data)
-      })
-  }, [])
-
-
+  const [result, setresult] = useState('');
   return (
     <>
-      {
-        result?.map((v, i) => {
-          return (
-            <>
-              <h1>{v.id}</h1>
-              <h4>{v.title}</h4>
-              <p>{v.body}</p>
-            </>
-          )
-        })
-      }
+      <Container>
+        <Form>
+          <InputGroup className='my-3'><Form.Control onChange={(e) => setresult(e.target.value)} placeholder='search name' /></InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.filter((val) => {
+              return result.toLowerCase() === ''
+                ? val
+                : val.first_name.toLowerCase().includes(result);
+            }).map((val, ind) => (
+              <tr key={ind}>
+                <td>{val.first_name}</td>
+                <td>{val.last_name}</td>
+                <td>{val.email}</td>
+                <td>{val.gender}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </>
   );
 }
